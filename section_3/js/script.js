dbc4fca1c0876cd3da01821e66bb20ba7d8a56bc
fukupage <4033990+fukupage@users.noreply.github.com>
2020-06-16 16:48:42 +0900

    6/16学習分

---
+++ section_3/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  //セレクタの指定とcssの操作
+  // $('#apple').css('color','red');
+  // $('#grape').css('color','purple');
+  // $('#calot').css('color','orange');
+  // $('.fruits').css('font-size','2rem');
+  // $('.vegetables').css('font-size','4rem');
+  $('#food > .fruits').css( 'font-size','2.5rem')
+  $('#food .fruits').css( 'background-color','#ccc')
+  $('.fruits,.main').css( 'color','#666')
+});
